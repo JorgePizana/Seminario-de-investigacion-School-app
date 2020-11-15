e3364cc03c9e53654897cf2299316d455cbc6d3d
Jorge Pizaña Mtz <a311049@uach.mx>
2020-11-15 02:35:34 -0700

    Added Navigation Bar
    Added Navigation Bar with Home, search, and profile icons
    Using an index to know when to change color depending on the tap (the selected item)

--- school_app_ux/lib/screens/home/index.dart
+++ school_app_ux/lib/screens/home/index.dart
@@ -8,6 +8,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen>
     with SingleTickerProviderStateMixin {
   TabController _tabController;
+  int _selectedItemIndex = 0;
 
   @override
   void initState() {
@@ -18,11 +19,52 @@ class _HomeScreenState extends State<HomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Row(
+        children: [
+          buildNavBarItem(Icons.home, 0),
+          buildNavBarItem(Icons.search, 1),
+          buildNavBarItem(Icons.person, 2),
+        ],
+      ),
       appBar: AppBar(
         title: Text(
           'Home screen',
           style: Theme.of(context).textTheme.headline6,
         ),
+      ),
+    );
+  }
+
+  Widget buildNavBarItem(IconData icon, int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedItemIndex = index;
+        });
+      },
+      child: Container(
+        height: 50,
+        width: MediaQuery.of(context).size.width / 3,
+        decoration: index == _selectedItemIndex
+            ? BoxDecoration(
+                border: Border(
+                    bottom: BorderSide(
+                  width: 4,
+                  color: Color.fromARGB(255, 201, 73, 85),
+                )),
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 201, 73, 85).withOpacity(0.3),
+                    Color.fromARGB(255, 201, 73, 85).withOpacity(0.010),
+                  ],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+                //color: index == _selectedItemIndex ? Color.fromARGB(255, 201, 73, 85) : Colors.white,
+              )
+            : BoxDecoration(),
+        child: Icon(icon,
+            color: index == _selectedItemIndex ? Colors.black : Colors.grey),
       ),
     );
   }
